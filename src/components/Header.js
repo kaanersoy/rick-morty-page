f4bb24ebb1116dbe24b73504c96784afe0db9781
Kaan Ersoy <57914492+kaanersoy12@users.noreply.github.com>
2020-11-17 19:41:47 +0300

    Compile Error Test
    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ export default class Header extends Component {
         return (
             <div className="header">
                 <ul>
-                    <li><a href="#"><img src={Logo} /></a></li>
-                    {/* <li><a href="#"><h1>{this.props.title}</h1></a></li> */}
-                    <li><a href="#"><h3>{this.props.secondary}</h3></a></li>
+                    <li><img src={Logo} alt="Rick And Morty Logo"/></li>
+                    <li><h3>{this.props.secondary}</h3></li>
                 </ul>
             </div>
         )
